42b3764f62fb40572185e35f1fef538c77ea9314
Rom Fradkin <fradkin.rom@gmail.com>
2022-07-23 23:50:42 -0500

    mobile index page updates

--- public_html/index.php
+++ public_html/index.php
@@ -4,19 +4,46 @@
 <!DOCTYPE html>
 <html lang="en-US">
     <head>
-        <title>Rom Fradkn</title>
+        <title>Rom Fradkin</title>
         <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
         <link rel="stylesheet" href="/index_desktop.css">
         <link rel="stylesheet" href="/index_mobile.css">
         <link rel="stylesheet" href="/fonts/fonts.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="format-detection" content="telephone=no">
     </head>
+    <script>
+        function open_mobile_overlay() {
+            document.getElementById("mobile_overlay").style.width = "100%";
+            document.getElementsByTagName("body")[0].style.overflow = "hidden";
+        }
+
+        function close_mobile_overlay() {
+            document.getElementById("mobile_overlay").style.width = "0%";
+            document.getElementsByTagName("body")[0].style.overflow = "auto";
+        }
+    </script>
     <body>
         <header>
-            <a href="/index.php" id="active_link">Home</a>
-            <a href="/about/about.php">About</a>
-            <a href="/notes_for_rom/notes_for_rom.php">Notes for Rom</a>
-            <a href="/about/about.php">Tor Blog</a>
+            <div id="desktop_navigation">
+                <a href="/index.php" id="active_link">Home</a>
+                <a href="/about/about.php">About</a>
+                <a href="/notes_for_rom/notes_for_rom.php">Notes for Rom</a>
+                <a href="/about/about.php">Tor Blog</a>
+            </div>
+            <div id="mobile_navigation">
+                <a href="HOMEPAGE" id="rom_fradkin_home_link">ROM FRADKIN.</a>
+                <div id="three_bars" onclick="open_mobile_overlay()">&#9776;</div>
+                <div id="mobile_overlay">
+                    <div id="cross" onclick="close_mobile_overlay()">&times;</div>
+                    <div id="mobile_links">
+                        <a href="/index.php" id="mobile_home">Home</a>
+                        <a href="/about/about.php" id="mobile_about">About</a>
+                        <a href="/notes_for_rom/notes_for_rom.php" id="mobile_notes_for_rom">Notes for Rom</a>
+                        <a href="/about/about.php" id="mobile_tor_blog">Tor Blog</a>
+                    </div>
+                </div>
+            </div>
         </header>
 
         <div id="image_bio">
@@ -35,7 +62,6 @@
 
             <div id="bio">
                 <img id="circle_image" src="/images/circle_image.png" alt="Circle Headshot">
-                <p class="bio_text" id="rom_fradkin_bio">Rom Fradkin</p>
                 <p class="bio_text" id="mit_bio">Massachusetts Institute of Technology</p>
                 <p class="bio_text" id="major_bio">Computer Science & Math</p>
                 <p class="bio_text" id="email_bio">fradkin.rom@gmail.com</p>
@@ -54,7 +80,8 @@
             <p id="welcome_content">
                 Come buy some Rom Fradkin merch:
                 <a target="_blank" rel="noopener noreferrer" href="https://www.redbubble.com/people/rfradkin/shop?asc=u">merch link</a>
-                (this is a joke but please buy something. it would make me laugh (and no, I don't get a commission)).
+                (this is a joke but please buy something. it would make me laugh (and no, I don't get a commission)). Also, this is 
+                not the ransomware link (trust. it goes to redbubble). I'm still working on that.
             </p>
         </div>
 
